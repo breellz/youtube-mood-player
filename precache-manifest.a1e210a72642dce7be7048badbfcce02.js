2945e9536c0553ad7f479ecc07c813db14fe3a5b
Bassit <barseetbrown@gmail.com>
2020-06-09 19:43:27 +0100

    Updates

---
+++ precache-manifest.a1e210a72642dce7be7048badbfcce02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f886db4e57f6cd8db484e21fc0a616",
+    "url": "/youtube-mood-player/index.html"
+  },
+  {
+    "revision": "8bfa48eeb5686abdc908",
+    "url": "/youtube-mood-player/static/css/main.463d1b71.chunk.css"
+  },
+  {
+    "revision": "beb97bbbe9598540e237",
+    "url": "/youtube-mood-player/static/js/2.e6c70c41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-mood-player/static/js/2.e6c70c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfa48eeb5686abdc908",
+    "url": "/youtube-mood-player/static/js/main.a01c531d.chunk.js"
+  },
+  {
+    "revision": "745511b57dce26c92643",
+    "url": "/youtube-mood-player/static/js/runtime-main.236e0fcb.js"
+  }
+]);
